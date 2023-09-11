b29079150ea183e5ff71dbdd682e423f142925bf
ptonini <pedro.tonini@hotmail.com>
2023-09-11 18:29:59 -0300

    fix: updated bucket version

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 module "bucket" {
-  source = "ptonini/s3-bucket/aws"
-  version = "~> 1.0.0"
-  name = var.bucket_name
+  source  = "ptonini/s3-bucket/aws"
+  version = "~> 1.3.0"
+  name    = var.bucket_name
   bucket_policy_statements = [
     {
-      Sid = "AWSCloudTrailAclCheck"
+      Sid    = "AWSCloudTrailAclCheck"
       Effect = "Allow"
       Principal = {
         Service = "cloudtrail.amazonaws.com"
       }
-      Action = "s3:GetBucketAcl"
+      Action   = "s3:GetBucketAcl"
       Resource = "arn:aws:s3:::${var.bucket_name}"
     },
     {
-      Sid = "AWSCloudTrailWrite"
+      Sid    = "AWSCloudTrailWrite"
       Effect = "Allow"
       Principal = {
         Service = "cloudtrail.amazonaws.com"
       }
-      Action = "s3:PutObject"
+      Action   = "s3:PutObject"
       Resource = "arn:aws:s3:::${var.bucket_name}/AWSLogs/${var.account_id}/*"
       Condition = {
         StringEquals = {
@@ -34,8 +34,8 @@ module "bucket" {
 }
 
 resource "aws_cloudtrail" "this" {
-  name = var.name
-  s3_bucket_name = module.bucket.this.id
+  name                          = var.name
+  s3_bucket_name                = module.bucket.this.id
   include_global_service_events = var.include_global_service_events
   depends_on = [
     module.bucket
